e8080d346fa4e19de019ac4cc985ecacf2f4d4c6
Rey Philip Regis <reyphilipregis@gmail.com>
2017-01-19 23:36:29 +0800

    Add installation of mongoose

--- scripts/ubuntu-web.sh
+++ scripts/ubuntu-web.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Install new version of Node
+# Install new version of NodeJS
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 
 # Install PM2 globally
 sudo npm install -y pm2 -g
+
+# Install Mongoose
+sudo npm install -y mongoose 
